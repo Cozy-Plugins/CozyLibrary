71b06c11eb495bff43437c9666188dc53f6ece86
Smudge <68658429+smuddgge@users.noreply.github.com>
2023-05-04 23:04:07 +0100

    Added condensed command

---
+++ src/main/java/com/github/cozyplugins/cozylibrary/command/command/CondensedCozyCommand.java
@@ -0,0 +1,47 @@
+package com.github.cozyplugins.cozylibrary.command.command;
+
+import com.github.cozyplugins.cozylibrary.command.datatype.CommandAliases;
+import com.github.cozyplugins.cozylibrary.command.datatype.CommandCredentials;
+import com.github.cozyplugins.cozylibrary.command.datatype.CommandPool;
+import com.github.cozyplugins.cozylibrary.pool.PermissionPool;
+import org.jetbrains.annotations.Nullable;
+
+public interface CondensedCozyCommand extends CozyCommand {
+
+    /**
+     * <h1>Used to get the commands credentials</h1>
+     *
+     * @return The command credentials.
+     */
+    @Nullable CommandCredentials getCredentials();
+
+    @Override
+    default @Nullable CommandAliases getAliases() {
+        if (this.getCredentials() == null) return null;
+        return this.getCredentials().getAliases();
+    }
+
+    @Override
+    default @Nullable String getDescription() {
+        if (this.getCredentials() == null) return null;
+        return this.getCredentials().getDescription();
+    }
+
+    @Override
+    default @Nullable String getSyntax() {
+        if (this.getCredentials() == null) return null;
+        return this.getCredentials().getSyntax();
+    }
+
+    @Override
+    default @Nullable PermissionPool getPermissionPool() {
+        if (this.getCredentials() == null) return null;
+        return this.getCredentials().getPermissionPool();
+    }
+
+    @Override
+    default @Nullable CommandPool getSubCommands() {
+        if (this.getCredentials() == null) return null;
+        return this.getCredentials().getCommandPool();
+    }
+}
