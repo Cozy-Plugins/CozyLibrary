72eef93a40d10c7d401abb7fa3a7d51de7637c05
Smudge <68658429+smuddgge@users.noreply.github.com>
2023-07-12 23:10:19 +0100

    Update Action.java

--- src/main/java/com/github/cozyplugins/cozylibrary/inventory/action/Action.java
+++ src/main/java/com/github/cozyplugins/cozylibrary/inventory/action/Action.java
@@ -2,6 +2,8 @@ package com.github.cozyplugins.cozylibrary.inventory.action;
 
 /**
  * <h1>Represents a inventory action</h1>
+ * Basic actions with 1 method can be interfaces.
+ * However, actions with multiple methods should be classes to simplify creating actions.
  */
 public interface Action {
 }
