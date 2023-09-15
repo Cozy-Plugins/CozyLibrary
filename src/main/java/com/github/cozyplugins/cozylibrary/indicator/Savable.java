d5c946f12a7ec34904c5b184e493df31b8f10be9
Smudge <68658429+smuddgge@users.noreply.github.com>
2023-09-15 16:31:53 +0100

    Added saveable indicator

---
+++ src/main/java/com/github/cozyplugins/cozylibrary/indicator/Savable.java
@@ -0,0 +1,12 @@
+package com.github.cozyplugins.cozylibrary.indicator;
+
+/**
+ * Indicates if a class is savable to storage.
+ */
+public interface Savable {
+
+    /**
+     * Used to save the class to storage.
+     */
+    void save();
+}
