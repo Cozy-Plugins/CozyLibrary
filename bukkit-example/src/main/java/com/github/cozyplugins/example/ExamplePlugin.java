9f11bbd4de27666a31ccfab7aae87a261573ceb6
Smudge <JXS1787@student.bham.ac.uk>
2025-02-27 23:54:19 +0000

    Fixed build fail - example plugin wasnt updated

--- bukkit-example/src/main/java/com/github/cozyplugins/example/ExamplePlugin.java
+++ bukkit-example/src/main/java/com/github/cozyplugins/example/ExamplePlugin.java
@@ -1,38 +1,11 @@
 package com.github.cozyplugins.example;
 
 import com.github.cozyplugins.cozylibrary.CozyPlugin;
-import com.github.cozyplugins.cozylibrary.command.CommandManager;
-import com.github.cozyplugins.cozylibrary.placeholder.PlaceholderManager;
 import org.jetbrains.annotations.NotNull;
 
 public class ExamplePlugin extends CozyPlugin<ExamplePluginLoader> {
 
     public ExamplePlugin(@NotNull ExamplePluginLoader plugin) {
-        super(plugin);
-    }
-
-    @Override
-    public boolean isCommandTypesEnabled() {
-        return false;
-    }
-
-    @Override
-    protected void onEnable() {
-
-    }
-
-    @Override
-    protected void onDisable() {
-
-    }
-
-    @Override
-    protected void onLoadCommands(@NotNull CommandManager commandManager) {
-
-    }
-
-    @Override
-    protected void onLoadPlaceholders(@NotNull PlaceholderManager<ExamplePluginLoader> placeholderManager) {
-
+        super(plugin, false);
     }
 }
