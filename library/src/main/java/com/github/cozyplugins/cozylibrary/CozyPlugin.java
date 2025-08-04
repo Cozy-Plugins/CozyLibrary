a86e1feeeeeca32fcfa806e58d2f2adf54027097
Smudge <JXS1787@student.bham.ac.uk>
2025-08-04 17:27:07 +0100

    Revert "Removed preloading nbt api"
    This reverts commit a6d55dbd4b32d292564ae23a0c95926333ffa41c.

--- library/src/main/java/com/github/cozyplugins/cozylibrary/CozyPlugin.java
+++ library/src/main/java/com/github/cozyplugins/cozylibrary/CozyPlugin.java
@@ -13,7 +13,6 @@ import com.github.cozyplugins.cozylibrary.placeholder.PlaceholderManager;
 import com.github.cozyplugins.cozylibrary.scoreboard.ScoreboardManager;
 import com.github.squishylib.configuration.directory.ConfigurationDirectory;
 import de.tr7zw.nbtapi.NBT;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -99,6 +98,13 @@ public abstract class CozyPlugin<P extends JavaPlugin> {
 
         // Before enabling cozy plugin.
         this.onBeforeEnable();
+
+        // Pre-load nbt api.
+        if (!NBT.preloadApi()) {
+            this.getPlugin().getLogger().warning("NBT-API wasn't initialized properly, disabling the plugin");
+            this.getPlugin().getPluginLoader().disablePlugin(this.getPlugin());
+            return this;
+        }
 
         // Set up default messages.
         this.messageConfig = new MessagesConfig(this.getPlugin().getDataFolder(), "messages.yml");
